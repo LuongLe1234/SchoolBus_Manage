4b11a10ba8289f051530553e74baba5caacd941e
le luong <luongletrong444@gmail.com>
2022-11-08 09:02:37 +0700

    update queries

---
+++ controller/managerController.js
@@ -0,0 +1,93 @@
+'use strict';
+
+
+const eventData = require('../data/managerEvents');
+
+const getAllEvents = async (req, res, next) => {
+    try {
+        const eventId = req.params.id;
+        const event = await eventData.getEvents(eventId);
+        res.send(event);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+const getallBusrouter = async (req, res, next) => {
+    try {
+        const eventId = req.params.id;
+        const event = await eventData.getallBusrouter(eventId);
+        res.send(event);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+const getallBusDriver = async (req, res, next) => {
+    try {
+        const eventId = req.params.id;
+        const event = await eventData.getallBusDriver(eventId);
+        res.send(event);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+
+
+const getEvent = async (req, res, next) => {
+    try {
+        const studentCode = req.params.id;
+        const event = await eventData.getById(studentCode);
+        res.send(event);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+const addUsers = async (req, res, next) => {
+    try {
+        const eventdata = req.body;
+        const insert = await eventData.createUser(eventdata);
+        res.send(insert);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+const getManager = async (req, res, next) => {
+    try {
+        const studentCode = req.params.id;
+        const event = await eventData.getById(studentCode);
+        res.send(event);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+const updatEvent = async (req, res, next) => {
+    try {
+        const eventId =  req.params.id;
+        const data = req.body;
+        const updated = await eventData.updateEvent(eventId, data);
+        res.send(updated);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+const deleteEvent = async (req, res, next) => {
+    try {
+        const eventId = req.params.id;
+        const deletedEvent = await eventData.deleteEvent(eventId);
+        res.send(deletedEvent);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+module.exports = {
+    getallBusDriver,
+    getallBusrouter,
+    getAllEvents,
+    getEvent,
+    addUsers,
+    updatEvent,
+    deleteEvent
+}
